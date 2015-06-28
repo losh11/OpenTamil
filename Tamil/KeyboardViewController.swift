c11e03c005bd3996594753a181a36e68d5e1bd4b
losh11 <loshan1212@gmail.com>
2015-06-28 02:40:43 +0100

    Better intergration method

--- Tamil/KeyboardViewController.swift
+++ Tamil/KeyboardViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class KeyboardViewController: UIInputViewController {
-
-    @IBOutlet var nextKeyboardButton: UIButton!
-
+    
+    // Declaration of layers
+    
+    @IBOutlet weak var frontRowOne: UIView!
+    @IBOutlet weak var frontRowTwo: UIView!
+    @IBOutlet weak var frontRowThree: UIView!
+    @IBOutlet weak var frontRowFour: UIView!
+    @IBOutlet weak var backRowOne: UIView!
+    @IBOutlet weak var backRowTwo: UIView!
+    @IBOutlet weak var backRowThree: UIView!
+    @IBOutlet weak var backRowFour: UIView!
+    
     override func updateViewConstraints() {
         super.updateViewConstraints()
     }
@@ -19,31 +28,59 @@ class KeyboardViewController: UIInputViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var keyboard = UINib(nibName: "defaultKeyboard", bundle: nil)
+        var keyboard = UINib(nibName: "testKeyboard", bundle: nil)
         var subset = keyboard.instantiateWithOwner(self, options: nil)
         view = subset[0] as! UIView;
+        
+        // Default layering configuration
+        
+        frontRowOne.hidden = false
+        frontRowTwo.hidden = false
+        frontRowThree.hidden = false
+        frontRowFour.hidden = false
+        backRowOne.hidden = true
+        backRowTwo.hidden = true
+        backRowThree.hidden = true
+        backRowFour.hidden = true
     }
-
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated
-//    }
-
-//    override func textWillChange(textInput: UITextInput) {
-        // The app is about to change the document's contents. Perform any preparation here.
-//    }
-
-//    override func textDidChange(textInput: UITextInput) {
-        // The app has just changed the document's contents, the document context has been updated.
     
-//        var textColor: UIColor
-//        var proxy = self.textDocumentProxy as! UITextDocumentProxy
-//        if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
-//            textColor = UIColor.whiteColor()
-//        } else {
-//            textColor = UIColor.blackColor()
-//        }
-//        self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
-//    }
-
+    @IBAction func didTapButton(sender: AnyObject) {
+        frontRowOne.hidden = true
+        frontRowTwo.hidden = true
+        frontRowThree.hidden = true
+        frontRowFour.hidden = true
+        backRowOne.hidden = false
+        backRowTwo.hidden = false
+        backRowThree.hidden = false
+        backRowFour.hidden = false
+    }
+    
+    // Logic programme
+    
+    @IBAction func logicSelectedOne(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func didTapButtonWithValue(sender: AnyObject) {
+        let button = sender.titleLabel!!.text
+        let title = button as String!
+        var proxy = textDocumentProxy as! UITextDocumentProxy
+        
+        switch title {
+        case "⇤" :
+            proxy.deleteBackward()
+        case "return" :
+            proxy.insertText("\n")
+        case "space" :
+            proxy.insertText(" ")
+        default :
+            proxy.insertText(title)
+        }
+    }
+    
+    @IBAction func didChangeKeyboard(sender: AnyObject) {
+        advanceToNextInputMode()
+    }
+    
 }
+
